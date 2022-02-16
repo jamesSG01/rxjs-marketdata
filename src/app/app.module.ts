9d7d0e3035b826abf454736334043a8473c3a381
James Nguyen <minhkhoix01@gmail.com>
2022-02-15 23:25:41 -0800

    Bet BTC going UP  0.1% within 10s
    For fun !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RealtimePriceComponent } from './realtime-price/realtime-price.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,11 @@ import { RealtimePriceComponent } from './realtime-price/realtime-price.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/realtime-price/realtime-price.component.ts
+++ src/app/realtime-price/realtime-price.component.ts
@@ -9,17 +9,61 @@ import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 export class RealtimePriceComponent implements OnInit {
   dataStream: WebSocketSubject<any> = webSocket('wss://ws.finnhub.io?token=c83rosiad3ide9hecq4g');
   cp: any;
+  win: any; 
+  lose: any; 
+  mid: any;
+  points: number = 0;
+  counter:number = 60;
+
   constructor() { }
 
   ngOnInit(): void {
-     this.dataStream.next({'type':'subscribe', 'symbol': 'BINANCE:LRCUSDT'});
+    this.dataStream.next({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'});
     //this.dataStream.next({'type':'subscribe-news', 'symbol': 'MSFT'});
     //this.dataStream.next({'type':'subscribe-news', 'symbol': 'AMZN'});
     
     this.dataStream.asObservable().subscribe((data:any) => {
       console.log("Subscriber got data >>>>> "+ JSON.stringify(data));
-      this.cp = JSON.parse(JSON.stringify(data)).data[0].p;
+      if (data.type !='ping')
+        this.cp = JSON.parse(JSON.stringify(data)).data[0].p;
     });
   }
-
+  startCountdown(seconds:any) {
+    let counter = seconds;
+      
+  }
+  game() {
+    this.win = (this.cp + this.cp*0.001);
+    this.lose = (this.cp - this.cp*0.001);
+    this.mid = this.cp;
+    console.log('Game started at price:'+this.cp)
+    this.counter = 10;// game is 60s 
+    const interval  = setInterval(() => {
+      this.counter--;
+      this.dataStream.asObservable().subscribe((data:any) => {
+        if (data.type !='ping')
+          this.cp = JSON.parse(JSON.stringify(data)).data[0].p;
+      });
+      console.log('Time left:' + this.counter+'| Win at:'+this.win+'| Lose at:'+this.lose );
+      if (this.cp < this.lose ) {
+        this.points-= 100;
+        clearInterval(interval);
+        console.log('You Lost!');
+      } else if (this.cp > this.win) {
+        console.log('You Won!');
+        this.points+= 150;
+        clearInterval(interval);
+      } 
+      if (this.counter <=0) {
+        if(this.cp >= this.mid){
+          console.log('You Won!');
+          this.points+= 150;
+        } else {
+          console.log('You Lost!');
+          this.points-= 100;
+        }
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 }
